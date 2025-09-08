6844b378d6b95899e6c45cc6f22b1596e45baa8a
Jason <xdos8213@gmail.com>
2025-09-08 20:05:20 +1000

    Add: Card component with basic css styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { BadgeCheck } from 'lucide-react'
 
-const Card = () => {
+type CardProps = {
+    title: string;
+    price: number;
+    features: string[];
+}
+const Card = ({title, price, features}: CardProps) => {
+
   return (
-    <div>Card</div>
+    <div className="w-80 h-[350px] flex flex-col justify-evenly bg-linear-to-bl from-violet-500 to-fuchsia-500  shadow-white-xl">
+        <p className="text-center text-text font-mono">{title}</p>
+        <div className="flex items-baseline justify-end-safe">
+            <p className="font-sans text-2xl font-bold text-a">${price}</p>
+            <span className="ml-2 mr-2 text-sm text-slate-800/70 font-serif ">/month</span>
+        </div>
+        <div className="flex justify-center">
+            
+            <div  className="flex flex-col  gap-2 text-white">
+                {features.map((feature, index) => (
+                    <div key={index} className="flex items-start gap-2">
+                        <div className="w-5 flex-shrink-0 flex justify-center">
+                        <BadgeCheck className="w-5 h-5 text-green-400" />
+                        </div>
+                    <p className="w-60 text-ellipsis ">{feature}</p>
+                </div>
+            ))}
+            </div>
+        
+        </div>
+    </div>
   )
 }
 
--- src/pages/Purchase/PurchasePage.tsx
+++ src/pages/Purchase/PurchasePage.tsx
@@ -1,8 +1,40 @@
+import Card from "../../components/Card";
+
+
 const PurchasePage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center
-                    text-text bg-background transition-colors duration-300">
-      Purchase Page
+                    text-text bg-background transition-colors duration-300 gap-12.5">
+      <Card title="Basic" price={19} features={
+        [
+          'Limited Project up to 9', 
+          'Limited AI support', 
+          'Boost Creativity and Productivity with 25 + AI tools including',
+          'Advanced analytics' 
+        ]
+
+      }>
+        
+      </Card>
+      
+      <Card title="Standard" price={29} features = {
+        ['Limited Project up to 49', 
+          'Boost Creativity and Productivity with 75 + AI tools',
+          'Marketing automations' ,
+          'Custom Domain'
+
+        ]
+      }>
+       </Card>
+
+      <Card title="Premium" price={39} features = {
+        [
+          'Unlimited products', 
+          'Unimited AI support', 
+          'Custom Integration' ,
+          'Priority phone support'
+        ]}>
+      </Card>
     </div>
   );
 };
